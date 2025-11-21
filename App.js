cc1491ad9a284f8134ae3094e41e78100223c1f3
Paulocome <paulocomeruth@gmail.com>
2025-11-21 18:08:03 +0200

    Refactor App.js to manage tasks with API calls

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
- {
-  "name": "mern-todo-frontend",
-  "version": "1.0.0",
-  "scripts": {
-    "start": "react-scripts start"
-  },
-  "dependencies": {
-    "axios": "^1.5.0",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "react-scripts": "5.0.1"
-  }
+ import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TaskList from './components/TaskList';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await axios.get('http://localhost:5000/api/tasks');
+    setTasks(res.data);
+  };
+
+  const addTask = async () => {
+    const res = await axios.post('http://localhost:5000/api/tasks', { title });
+    setTasks([...tasks, res.data]);
+    setTitle('');
+  };
+
+  const toggleTask = async (task) => {
+    const res = await axios.put(`http://localhost:5000/api/tasks/${task._id}`, { completed: !task.completed });
+    setTasks(tasks.map(t => t._id === task._id ? res.data : t));
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:5000/api/tasks/${id}`);
+    setTasks(tasks.filter(t => t._id !== id));
+  };
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>To-Do List</h1>
+      <input value={title} onChange={e => setTitle(e.target.value)} placeholder="New task" />
+      <button onClick={addTask}>Add</button>
+      <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
+    </div>
+  );
 }
+
+export default App;
